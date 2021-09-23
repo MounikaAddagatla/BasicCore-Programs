5f5072d5e412891493ad21a377bf015e71c91bc6
mounika <mounaadagatla@gmail.com>
2021-09-23 12:05:30 +0530

    [Mounika] Added: a new class

---
+++ CharExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicLogicPrograms
+{
+    class CharExample
+    {
+        /// <summary>
+        /// charecter intialization
+        /// </summary>
+        char ch;
+        public void VowelOrConsonant()
+        {
+            Console.WriteLine("Enter any character: ");
+            ch = Convert.ToChar(Console.ReadLine());
+            // Condition for vowel checking
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + " is Vowel.");
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is Consonant.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
